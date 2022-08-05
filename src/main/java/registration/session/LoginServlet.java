07ab9a69f8e8544b27de57299f0a5005245ba795
Arthur <arturshatailo@gmail.com>
2022-08-05 19:29:21 +0300

    admin panel deployment branch

--- src/main/java/registration/session/LoginServlet.java
+++ src/main/java/registration/session/LoginServlet.java
@@ -24,7 +24,7 @@ public class LoginServlet extends HttpServlet implements InstanceRepository, Coo
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("/registration/login.jsp");
+        resp.sendRedirect("/login.jsp");
     }
 
     /**
