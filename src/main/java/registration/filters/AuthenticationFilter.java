64645b560cde936524071067c135cf032c37bbcc
Arthur <arturshatailo@gmail.com>
2022-08-07 22:34:06 +0300

    filter adjustment admin panel deployment branch

--- src/main/java/registration/filters/AuthenticationFilter.java
+++ src/main/java/registration/filters/AuthenticationFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter("/*")
@@ -27,14 +28,19 @@ public class AuthenticationFilter implements Filter, CookieFactory {
 
         String uri = req.getRequestURI();
 
-        User sessionObject = (User) req.getSession(false);
+        HttpSession session = req.getSession();
+        User sessionObject = (User) session.getAttribute("user");
+        User sessionObjectAdmin = (User) session.getAttribute("admin");
 
-        if (sessionObject == null && !(uri.endsWith("/home.jsp")
+        if (sessionObject == null && sessionObjectAdmin == null && !(uri.endsWith("/home.jsp")
                 || uri.endsWith("/loginUser")
                 || uri.endsWith("/login.jsp")
                 || uri.endsWith("/registerUser")
                 || uri.endsWith("/registration.jsp")
-                || uri.endsWith(".com"))) {
+                || uri.endsWith(".com")
+                || uri.endsWith("login-admin.jsp")
+                || uri.endsWith("/loginAdmin"))) {
+
             this.context.log("<<< Unauthorized access request");
             setCookie(res, "errorMessage", "No access", 5);
             res.sendRedirect("/login.jsp");
